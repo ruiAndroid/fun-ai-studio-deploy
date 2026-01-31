fa5a79348a9e20a7516e9877ef9a5950043baf42
jianrui <jianrui@fun.tv>
2026-02-01 01:27:26 +0800

    补充提交

--- src/main/java/fun/ai/studio/FunAiStudioDeployApplication.java
+++ src/main/java/fun/ai/studio/FunAiStudioDeployApplication.java
@@ -2,8 +2,10 @@ package fun.ai.studio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FunAiStudioDeployApplication {
     public static void main(String[] args) {
         SpringApplication.run(FunAiStudioDeployApplication.class, args);
